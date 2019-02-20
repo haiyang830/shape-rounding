b274a113ac147040fe737dcf3a8e73116260c721
haiyang <45539007+haiyang830@users.noreply.github.com>
2019-02-20 09:49:04 +0800

    Update shape-rounding.lua

--- shape-rounding.lua
+++ shape-rounding.lua
@@ -7,7 +7,7 @@ script_description = tr"四舍五入取整矢量图形代码中的小数"
 script_author = "haiyang"
 script_version = "1"
 
-function round_numbers(subs, sel)
+function shape_rounding(subs, sel)
     for _, i in ipairs(sel) do
         local line = subs[i]
         -- aegisub.debug.out(i..": "..line.text.."\n")
@@ -15,32 +15,32 @@ function round_numbers(subs, sel)
         line.text = newText
         subs[i] = line
     end
-    aegisub.set_undo_point(tr"round_numbers")
+    aegisub.set_undo_point(tr"shape_rounding")
 end
 
 function cleanStr(str)
 	local newStr
-	newStr= matchStr(
-	str,
-	"m ([%-%d%.mnlbspc ]+)", -- re_str
-	"([%-%d%.mnlbspc]+)",	 -- re_str_split
-	" " -- concat_sep
+	newStr = matchStr(
+		str,
+		"m ([%-%d%.mnlbspc ]+)", -- re_str
+		"([%-%d%.mnlbspc]+)",	 -- re_str_split
+		" " -- concat_sep
 	)
 	newStr = matchStr(
-	newStr,
-	"clip%(([%-%d%.%,]+)%)",
-	"([%-%d%.]+)",
-	","
+		newStr,
+		"clip%(([%-%d%.%,]+)%)",
+		"([%-%d%.]+)",
+		","
 	)	
     return newStr
 end
 
-function matchStr(str,re_str,re_str_split,concat_sep)
+function matchStr(str, re_str, re_str_split, concat_sep)
 	local newStr = str
-    for drawStr in string.gmatch(newStr,re_str) do
+    for drawStr in string.gmatch(newStr, re_str) do
 		local Str_table = {}
 		local i = 1
-		for numStr in string.gmatch(drawStr,re_str_split) do
+		for numStr in string.gmatch(drawStr, re_str_split) do
 			Str_table[i] = tonumber(numStr) or numStr
 			i = i + 1
 		end
@@ -50,18 +50,17 @@ function matchStr(str,re_str,re_str_split,concat_sep)
 				Str_table[k] = resNum
 			end
 		end
-		drawStr = drawStr:gsub("%-","%%%-")
-		newStr=newStr:gsub(drawStr,table.concat(Str_table,concat_sep))
-    end
-    return newStr
+		drawStr = drawStr:gsub("%-", "%%%-")
+		newStr = newStr:gsub(drawStr, table.concat(Str_table,concat_sep))
+	end
+	return newStr
 end
 
 function round(x)
-    if x%2 ~= 0.5
-    then
-        return math.floor(x+0.5)
-    end
-    return x-0.5
+    if x%2 ~= 0.5 then
+		return math.floor(x+0.5)
+	end
+	return x-0.5
 end
 
-aegisub.register_macro(script_name, script_description, round_numbers)
+aegisub.register_macro(script_name, script_description, shape_rounding)
